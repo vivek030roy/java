99542da77afc3d91b386ea9b148767796913fff8
vivek030roy <vivekanand020301@gmail.com>
2025-04-11 15:11:14 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class assign {
+  public static void main(String[] args){
+
+    int x=5;
+    System.out.println(x);
+    x++;
+    System.out.println(x);
+    x--;
+    System.out.println(x);
+  }
+}
