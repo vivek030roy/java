937cc94ba854e8c26385d85386a80a9623a702d1
vivek030roy <vivekanand020301@gmail.com>
2025-04-18 20:39:26 +0530

    Add files via upload

---
+++ poscheck.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class poscheck {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the no.");
+    int n = sc.nextInt();
+    if(n>0)
+    System.out.println("No is positive");
+    else
+    System.out.println("No. is negative");
+
+}
+}
